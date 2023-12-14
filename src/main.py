28de40d257157cf285536a6208ff3842762f357f
buddhisenarathna <buddhi.senarathna@kodez.com.au>
2023-12-14 22:45:46 +0530

    Add azure cognito search index creation code

--- src/main.py
+++ src/main.py
@@ -18,22 +18,34 @@ from llama_index import (
 # Example that reads the pages with the `page_ids`
 from llama_hub.confluence import ConfluenceReader
 
+from llama_index.vector_stores import CognitiveSearchVectorStore
+from llama_index.vector_stores.cogsearch import (
+    IndexManagement,
+    MetadataIndexFieldType,
+    CognitiveSearchVectorStore,
+)
+
+# set up Azure Cognitive Search
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents import SearchClient
+from azure.core.credentials import AzureKeyCredential
+
 
 def run():
     llm = AzureOpenAI(
         engine=AZURE_OPENAI_CHAT_DEPLOYEMENT, model=AZURE_OPENAI_CHAT_MODEL, temperature=0.7, top_p=0.95
     )
-    messages = [
-        ChatMessage(
-            role="system", content="You are a pirate with colorful personality."
-        ),
-        ChatMessage(
-            role="user", content="Where did you hide the ship?"
-        )
-    ]
+    # messages = [
+    #     ChatMessage(
+    #         role="system", content="You are a pirate with colorful personality."
+    #     ),
+    #     ChatMessage(
+    #         role="user", content="Where did you hide the ship?"
+    #     )
+    # ]
 
-    res = llm.chat(messages)
-    print(res)
+    # res = llm.chat(messages)
+    # print(res)
     llm_predictor = LLMPredictor(llm=llm)
 
     embed_model = AzureOpenAIEmbedding(
@@ -61,32 +73,92 @@ def run():
     
     set_global_service_context(service_context)
 
+    # page_ids = ["232325167"]
+    # space_key = "CYB"
+    # reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
+    # documents = reader.load_data(space_key=space_key, include_attachments=False, page_status="current")
+    # documents.extend(reader.load_data(page_ids=page_ids, include_children=False, include_attachments=False))
 
-    page_ids = ["232325167"]
-    space_key = "CYB"
-    reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
-    documents = reader.load_data(space_key=space_key, include_attachments=False, page_status="current")
-    documents.extend(reader.load_data(page_ids=page_ids, include_children=False, include_attachments=False))
-    index = VectorStoreIndex.from_documents(documents,show_progress=True)
-    # store it for later
-    index.storage_context.persist()
+    cognitive_search_credential = AzureKeyCredential(AZURE_COGNITIVE_SEARCH_APIKEY)
 
-    # check if storage already exists
+    service_endpoint = AZURE_COGNITIVE_SEARCH_ENDPOINT
+
+    # Use index client to demonstrate creating an index
+    index_client = SearchIndexClient(
+        endpoint=service_endpoint,
+        credential=cognitive_search_credential,
+    )
+
+    # Use search client to demonstration using existing index
+    # search_client = SearchClient(
+    #     endpoint=service_endpoint,
+    #     index_name=AZURE_COGNITIVE_SEARCH_INDEX_NAME,
+    #     credential=cognitive_search_credential,
+    # )
+
+    vector_store = CognitiveSearchVectorStore(
+        search_or_index_client=index_client,
+        index_name=AZURE_COGNITIVE_SEARCH_INDEX_NAME,
+        index_management=IndexManagement.CREATE_IF_NOT_EXISTS,
+        id_field_key="id",
+        chunk_field_key="content",
+        embedding_field_key="embedding",
+        metadata_string_field_key="li_jsonMetadata",
+        doc_id_field_key="li_doc_id",
+    )
+
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    service_context = ServiceContext.from_defaults(
+        llm=llm, embed_model=embed_model, chunk_size=2000, chunk_overlap=0.3
+    )
+
+    index = VectorStoreIndex.from_documents(
+        [],
+        storage_context=storage_context,
+        service_context=service_context,
+        show_progress=True,
+    )
+
+    confluence_spaces = CONFLUENCE_SPACE_LIST.split(",")
+
+    for space_key in confluence_spaces:
+        reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
+        documents = reader.load_data(space_key=space_key, include_attachments=False, page_status="current")
+        documents.extend(reader.load_data(page_ids=[], include_children=False, include_attachments=False))
+        for doc in documents:
+            print(doc.doc_id)
+            index.insert(doc)
+
+    # index = VectorStoreIndex.from_documents(documents,show_progress=True)
+    # index.storage_context.persist()
+
+    #############old#########################
+
+    # # check if storage already exists
     # if not os.path.exists("./storage"):
     #      # load the documents and create the index
 
     #     # reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
-    #     # cql=f'type="page" AND space = "Workspaces" AND title = "Architecture"'
+    #     # cql=f'type=page and label='
     #     # documents = reader.load_data(cql=cql, max_num_results=5)
     #     # cursor = reader.get_next_cursor()
     #     # documents.extend(reader.load_data(cql=cql, cursor=cursor, max_num_results=5))
 
-    #     page_ids = ["237699280"]
-    #     space_key = "HWW"
-    #     reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
-    #     documents = reader.load_data(space_key=space_key, include_attachments=False, page_status="current")
-    #     documents.extend(reader.load_data(page_ids=page_ids, include_children=False, include_attachments=False))
-    #     index = VectorStoreIndex.from_documents(documents,show_progress=True)
+    #     index = VectorStoreIndex.from_documents([])
+    #     # page_ids = []
+    #     # space_key = "HWW"
+
+    #     confluence_spaces = CONFLUENCE_SPACE_LIST.split(",")
+
+    #     for space_key in confluence_spaces:
+    #         reader = ConfluenceReader(base_url=CONFLUENCE_BASE_URL)
+    #         documents = reader.load_data(space_key=space_key, include_attachments=False, page_status="current")
+    #         documents.extend(reader.load_data(page_ids=[], include_children=False, include_attachments=False))
+    #         for doc in documents:
+    #             print(doc.doc_id)
+    #             index.insert(doc)
+
+        
     #     # store it for later
     #     index.storage_context.persist()
     # else:
@@ -100,8 +172,8 @@ def run():
     #     ),
     # ]
 
-    chat_engine = index.as_query_engine()
-    response = chat_engine.query("What are the tools for testing?")
+    chat_engine = index.as_query_engine(similarity_top_k=2)
+    response = chat_engine.query("What is cybermity")
     print(response)
     print(response.source_nodes)
 
@@ -113,6 +185,7 @@ if __name__ == "__main__":
     CONFLUENCE_PASSWORD  = os.getenv("CONFLUENCE_PASSWORD")
     CONFLUENCE_USERNAME = os.getenv("CONFLUENCE_USERNAME")
     CONFLUENCE_BASE_URL = os.getenv("CONFLUENCE_BASE_URL")
+    CONFLUENCE_SPACE_LIST = os.getenv("CONFLUENCE_SPACE_LIST")
 
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
@@ -123,5 +196,9 @@ if __name__ == "__main__":
     AZURE_OPENAI_EMBEDDING_MODEL = os.getenv("AZURE_OPENAI_EMBEDDING_MODEL")
     AZURE_OPENAI_EMBEDDING_DEPLOYEMENT = os.getenv(
         "AZURE_OPENAI_EMBEDDING_DEPLOYEMENT")
+    
+    AZURE_COGNITIVE_SEARCH_ENDPOINT = os.getenv("AZURE_COGNITIVE_SEARCH_ENDPOINT")
+    AZURE_COGNITIVE_SEARCH_APIKEY = os.getenv("AZURE_COGNITIVE_SEARCH_APIKEY")
+    AZURE_COGNITIVE_SEARCH_INDEX_NAME = os.getenv("AZURE_COGNITIVE_SEARCH_INDEX_NAME")
 
     run()
